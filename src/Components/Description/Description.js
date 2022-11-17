330d57c30a15a5927598c418a45b8d0dfdead241
hiramr97 <hiram.rodriguez97@icloud.com>
2022-11-16 22:19:51 -0500

    finally got the api to work with each individual pokemon!

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// import data from "../Data/data.json";
 import Lane from "./Lane";
 import Build from "./Build";
 import Build1 from "./Build1";
@@ -9,6 +8,7 @@ import { useParams } from "react-router-dom";
 
 const Description = () => {
   const [data, setData] = useState();
+  const [error, setError] = useState();
   const { id } = useParams();
 
   const options = {
@@ -25,39 +25,42 @@ const Description = () => {
       options
     )
       .then((response) => response.json())
-      .then((response) => setData(response))
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => setError(error));
   };
 
   useEffect(() => {
     fetchPokemon();
-  }, [])
-  
-  console.log(data)
-  // let newData = Object.values(data)
-  // const pictureData = +data.id+ - 1;
-  // if (newData[2] === id) {
-  //   return (
-  //     <div>
-  //       <h1 className="name">{data.display_name}</h1>
-  //       <Lane />
-  //       <h3 className="descriptionStyling">Role</h3>
-  //       <h3 className="descriptionStyling">{data.tags.role}</h3>
-  //       <h3 className="descriptionStyling">Difficulty</h3>
-  //       <h3 className="descriptionStyling">{data.tags.difficulty}</h3>
-  //       <h3 className="descriptionStyling">Recommended Build</h3>
-  //       <div className="Container-Build">
-  //         <Build />
-  //         <Build1 />
-  //         <Build2 />
-  //       </div>
-  //       <img
-  //         className="DescriptionPokemon"
-  //         src={pictures[pictureData].image}
-  //         alt=""
-  //       />
-  //     </div>
-  //   );
-  // }
+  }, []);
 
+  if (data === undefined) return;
+
+  let newData = Object.values(data);
+  const pictureData = +data.id + -1;
+  if (newData[2] === id) {
+    return (
+      <div>
+        <h1 className="name">{data.display_name}</h1>
+        <Lane />
+        <h3 className="descriptionStyling">Role</h3>
+        <h3 className="descriptionStyling">{data.tags.role}</h3>
+        <h3 className="descriptionStyling">Difficulty</h3>
+        <h3 className="descriptionStyling">{data.tags.difficulty}</h3>
+        <h3 className="descriptionStyling">Recommended Build</h3>
+        <div className="Container-Build">
+          <Build />
+          <Build1 />
+          <Build2 />
+        </div>
+        <img
+          className="DescriptionPokemon"
+          src={pictures[pictureData].image}
+          alt=""
+        />
+      </div>
+    );
+  }
 };
 export default Description;
